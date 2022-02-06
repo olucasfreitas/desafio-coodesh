4f507e15048e215c006b57807843baa533914658
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-02-06 20:26:27 -0300

    added necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,31 @@ import { SearchPageComponent } from './search-page/search-page.component';
 import { InfoCardComponent } from './components/info-card/info-card.component';
 import { InfoModalComponent } from './components/info-modal/info-modal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
   declarations: [AppComponent, SearchPageComponent, InfoCardComponent, InfoModalComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
