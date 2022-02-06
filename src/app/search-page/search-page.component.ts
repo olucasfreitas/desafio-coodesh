6bd59247e44ba497d3ba211c968e30943f8668ea
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-02-06 20:28:06 -0300

    created methods for search, pagination and sort items

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Article } from '../entities/article.entity';
+import { ArticleService } from '../services/article.service';
 
 @Component({
   selector: 'app-search-page',
@@ -6,7 +9,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-page.component.scss'],
 })
 export class SearchPageComponent implements OnInit {
-  constructor() {}
+  articles: Article[] = [];
+  limit = 10;
+  startFrom = 0;
+  loading = false;
+  sort = '';
+  sortFilter = '';
+  operator = '';
+  searchForm = this.formBuilder.group({
+    name: ['', [Validators.required]],
+  });
 
-  ngOnInit(): void {}
+  constructor(private articleService: ArticleService, private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loading = !this.loading;
+    this.articleService
+      .getArticles(this.limit, this.startFrom, undefined, undefined, undefined, undefined)
+      .subscribe((data) => {
+        this.articles = data;
+        this.loading = !this.loading;
+      });
+  }
+
+  loadMoreArticles(): void {
+    this.loading = !this.loading;
+    this.startFrom += this.limit;
+    this.articleService
+      .getArticles(this.limit, this.startFrom, this.sort, 'title', this.searchForm.value.name, 'contains')
+      .subscribe((data) => {
+        data.forEach((article) => {
+          this.articles.push(article);
+        });
+        this.loading = !this.loading;
+      });
+  }
+
+  sortFromOlder(): void {
+    this.loading = !this.loading;
+    this.sort = 'publishedAt:asc';
+    this.startFrom = 0;
+    this.articleService
+      .getArticles(this.limit, this.startFrom, this.sort, this.sortFilter, this.searchForm.value.name, this.operator)
+      .subscribe((data) => {
+        this.articles = data;
+        this.loading = !this.loading;
+      });
+  }
+
+  sortFromNewer(): void {
+    this.loading = !this.loading;
+    this.sort = 'publishedAt:desc';
+    this.startFrom = 0;
+    this.articleService
+      .getArticles(this.limit, this.startFrom, this.sort, this.sortFilter, this.searchForm.value.name, this.operator)
+      .subscribe((data) => {
+        this.articles = data;
+        this.loading = !this.loading;
+      });
+  }
+
+  search(): void {
+    this.loading = !this.loading;
+    this.startFrom = 0;
+    this.sortFilter = 'title';
+    this.operator = 'contains';
+    this.articleService
+      .getArticles(this.limit, this.startFrom, this.sort, this.sortFilter, this.searchForm.value.name, this.operator)
+      .subscribe((data) => {
+        this.articles = data;
+        this.loading = !this.loading;
+      });
+  }
 }
