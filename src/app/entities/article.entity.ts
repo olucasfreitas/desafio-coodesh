0ed2241d216e18c6962b7387eb17437750e37433
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-02-06 20:27:14 -0300

    created article entity

---
+++ src/app/entities/article.entity.ts
@@ -0,0 +1,13 @@
+export class Article {
+  id = '';
+  title = '';
+  url = '';
+  imageUrl = '';
+  newsSite = '';
+  summary = '';
+  publishedAt = '';
+  updatedAt = '';
+  featured = false;
+  launches = [{ id: '', provider: '' }];
+  events = [{ id: '', provider: '' }];
+}
