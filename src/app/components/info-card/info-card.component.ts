1382c6bd0dfbfd427bef2a303843c060af9390dd
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-02-06 20:25:35 -0300

    configured info card to recieve info and open modal

--- src/app/components/info-card/info-card.component.ts
+++ src/app/components/info-card/info-card.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Article } from 'src/app/entities/article.entity';
+import { InfoModalComponent } from '../info-modal/info-modal.component';
 
 @Component({
   selector: 'app-info-card',
   templateUrl: './info-card.component.html',
   styleUrls: ['./info-card.component.scss'],
 })
-export class InfoCardComponent implements OnInit {
-  constructor() {}
+export class InfoCardComponent {
+  @Input() articleInfo = new Article();
+  constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {}
+  openModal(): void {
+    this.dialog.open(InfoModalComponent, {
+      height: '50%',
+      width: '75%',
+      data: this.articleInfo.id,
+    });
+  }
 }
