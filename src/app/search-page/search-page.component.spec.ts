e8ea3d5783808a8394629995ddf65ad1ed612c1d
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-02-06 21:13:02 -0300

    configured and added unit tests to search page

--- src/app/search-page/search-page.component.spec.ts
+++ src/app/search-page/search-page.component.spec.ts
@@ -1,24 +1,77 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchPageComponent } from './search-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ArticleService } from '../services/article.service';
+import { of } from 'rxjs';
+import { Article } from '../entities/article.entity';
 
 describe('SearchPageComponent', () => {
   let component: SearchPageComponent;
   let fixture: ComponentFixture<SearchPageComponent>;
+  let articleService: jasmine.SpyObj<ArticleService>;
 
   beforeEach(async () => {
+    const articleServiceSpy = jasmine.createSpyObj('ArticleService', ['getArticles']);
+
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [SearchPageComponent],
+      providers: [{ provide: ArticleService, useValue: articleServiceSpy }],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchPageComponent);
     component = fixture.componentInstance;
+    articleService = TestBed.inject(ArticleService) as jasmine.SpyObj<ArticleService>;
+    articleService.getArticles.and.returnValue(of([new Article()]));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load articles onto page', () => {
+    articleService.getArticles.and.returnValue(of([new Article()]));
+    component.ngOnInit();
+    expect(articleService.getArticles).toHaveBeenCalled();
+    expect(component.articles).toEqual([new Article()]);
+  });
+
+  it('should load more articles onto page', () => {
+    articleService.getArticles.and.returnValue(of([new Article()]));
+    component.loadMoreArticles();
+    expect(articleService.getArticles).toHaveBeenCalled();
+    expect(component.articles).toContain(new Article());
+    expect(component.startFrom).toEqual(10);
+  });
+
+  it('should load articles from oldest to newest', () => {
+    articleService.getArticles.and.returnValue(of([new Article()]));
+    component.sortFromOlder();
+    expect(articleService.getArticles).toHaveBeenCalled();
+    expect(component.articles).toContain(new Article());
+    expect(component.startFrom).toEqual(0);
+    expect(component.sort).toEqual('publishedAt:asc');
+  });
+
+  it('should load articles from oldest to newest', () => {
+    articleService.getArticles.and.returnValue(of([new Article()]));
+    component.sortFromNewer();
+    expect(articleService.getArticles).toHaveBeenCalled();
+    expect(component.articles).toContain(new Article());
+    expect(component.startFrom).toEqual(0);
+    expect(component.sort).toEqual('publishedAt:desc');
+  });
+
+  it('should search for article that contain the input value', () => {
+    articleService.getArticles.and.returnValue(of([new Article()]));
+    component.search();
+    expect(articleService.getArticles).toHaveBeenCalled();
+    expect(component.articles).toContain(new Article());
+    expect(component.startFrom).toEqual(0);
+    expect(component.sortFilter).toEqual('title');
+    expect(component.operator).toEqual('contains');
   });
 });
